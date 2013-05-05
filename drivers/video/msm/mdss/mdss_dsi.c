43fcf8add2f6beb0dc5d0739b0f46f874a8f7be9
Se Hun Kim <sean2.kim@lge.com>
2013-05-05 15:33:46 -0700

    msm: mdss: Adding lcd notifier
    Supports notifier for start&end of LCD power on&off
    
    Change-Id: Idaa685a2c554286da5f764519305d830c4d1a1bd
    Signed-off-by: Se Hun Kim <sean2.kim@lge.com>
    Signed-off-by: Jongrak Kwon <jongrak.kwon@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -22,6 +22,7 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 #include <mach/board_lge.h>
+#include <linux/lcd_notify.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -1379,6 +1380,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -1389,8 +1391,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata);
 		break;
@@ -1399,6 +1403,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 #if defined(CONFIG_LGE_MIPI_DZNY_JDI_INCELL_FHD_VIDEO_PANEL)
         if(touch_driver_registered){
             touch_notifier_call_chain(LCD_EVENT_TOUCH_LPWG_ON, NULL);
